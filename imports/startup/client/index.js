0e50d29294b3f2b0d224f45b22080bbd1f991f17
Renato Teixeira <rena2damas@gmail.com>
2018-09-15 12:27:05 +0200

    Header

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,3 +1,7 @@
 // Import client startup through a single index entry point
-
 import './routes.js';
+
+Template.Header.rendered = function() {
+    import './vendor/modernizr-2.6.2.min'
+    import './vendor/jquery.zoomslider.min'
+};
