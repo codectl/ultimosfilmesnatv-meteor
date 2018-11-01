a2255d494e3b67596593c157ca35566257510f12
Renato Teixeira <rena2damas@gmail.com>
2018-11-01 08:45:17 +0100

    Reading movie from db

--- imports/api/movies/movies.js
+++ imports/api/movies/movies.js
@@ -1,4 +1,4 @@
 // Definition of the links collection
 import { Mongo } from 'meteor/mongo';
 
-export const Movies = new Mongo.Collection('movies');
+export const Movies = new Mongo.Collection('movie');
--- imports/ui/components/movies_board/movies_board.js
+++ imports/ui/components/movies_board/movies_board.js
@@ -1,12 +1,19 @@
 import { Template } from 'meteor/templating'
+import { Meteor } from "meteor/meteor";
+import { Movies } from "/imports/api/movies/movies";
 
 import './movies_board.html';
 
 import '../date_filter/date_filter.js';
 import '../movie/movie.js';
 
-Template.body.helpers({
-    movies: function(){
+Template.movies_board.onCreated(function () {
+    Meteor.subscribe('movies.all');
+});
+
+Template.movies_board.helpers({
+    getMovies() {
+        console.log(Movies.find({}));
         return Movies.find({});
     }
 });
